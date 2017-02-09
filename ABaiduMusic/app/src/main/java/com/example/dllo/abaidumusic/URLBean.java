518996ba83c9ecf318783db90eb23f94ab090dc7
zyswyq <763958570@qq.com>
2017-02-09 18:36:23 +0800

    no message

---
+++ ABaiduMusic/app/src/main/java/com/example/dllo/abaidumusic/URLBean.java
@@ -0,0 +1,37 @@
+package com.example.dllo.abaidumusic;
+
+/**
+ * Created by dllo on 17/2/9.
+ * 在这里存储所有接口
+ */
+
+public class URLBean {
+
+    /**
+     * 音乐 --K歌轮播图
+     *http://tingapi.ting.baidu.com/v1/restserver/ting?from=android&version=5.9.8.1&channel=1426d&operator=3&method=baidu.ting.active.showList
+     *
+     * 动态  list 数据
+     * http://tingapi.ting.baidu.com/v1/restserver/ting?from=android&version=5.9.8.1&channel=1426d&operator=3&method=baidu.ting.ugcfriend.getList&format=json&param=Vzj1F5tjOnKDcBtupE2xkK1h0%2FBmOlwiPpt7MdO6UD0urZf0rC9NZeQrfa2qCikoGqN8AC%2FEYvNNLX4LkJH%2BjA%3D%3D&timestamp=1486632418&sign=eb7efd3c076c7f7c4cd2402b364d9c2f
+     *
+     *  发现  以及发现二级界面分类的图标
+     *  http://tingapi.ting.baidu.com/v1/restserver/ting?from=android&version=5.9.8.1&channel=1426d&operator=3&method=baidu.ting.plaza.indexFind&param=WV%2FB0OohBbFuGg0xKpldarz7CAZJJXK4auqEmfBO1bA7r5yF%2F1ts1cMw%2FqXkgAkH7eNZQOWL5ksdBTJ6dz5gNxIRZboV1KztZxa0%2F3Zsdi4%3D&timestamp=1486632421&sign=2ed28fc410e34aba61e0a7f5369bc02f
+     *
+     *  音乐  --- 歌单  --分类
+     *  http://tingapi.ting.baidu.com/v1/restserver/ting?from=android&version=5.9.8.1&channel=1426d&operator=3&method=baidu.ting.ugcdiy.getChannels&param=yJxNHh3bMIYFcEaQxwK83tq5%2F5DBqbVtMHuq1g4fxssjKoOo9wn%2Bn5pjNDi%2FJFLa&timestamp=1486632431&sign=256a5151e9fd6bcbeef0b6f42b5c1919
+     *
+     *  音乐 ---歌单  --热门数据
+     *  http://tingapi.ting.baidu.com/v1/restserver/ting?from=android&version=5.9.8.1&channel=1426d&operator=3&method=baidu.ting.ugcdiy.getChanneldiy&param=aUeJzjQd0Bxv60bsMl1nzvktSPLjc5EcGxFAt77r3ORvfYOi0G0UiMU15Gu9rmiLXpXaecx%2BVhS3VNWrDDHaz%2FdPLIB52H4GjQR8wkaLFrrkLECGMiGJkF9toxnAK5KX&timestamp=1486632431&sign=39afa305d4a82eb67ac1f191f47f64e6
+     *
+     *  音乐 ---榜单 数据
+     *  http://tingapi.ting.baidu.com/v1/restserver/ting?from=android&version=5.9.8.1&channel=1426d&operator=3&method=baidu.ting.billboard.billCategory&format=json&kflag=2
+     *
+     *  音乐 ---场景电台 --语言  (最后的数字变化)
+     *  http://tingapi.ting.baidu.com/v1/restserver/ting?from=android&version=5.9.8.1&channel=1426d&operator=3&method=baidu.ting.scene.getCategoryScene&category_id=4
+     *
+     *  发现--直播二级界面 (最后的数字变化)
+     *  http://tingapi.ting.baidu.com/v1/restserver/ting?from=android&version=5.9.8.1&channel=1426d&operator=3&method=baidu.ting.show.item&page_size=30&page_no=1&category=4
+     *
+     */
+
+}
