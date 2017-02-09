820cc82cd65e7e9e564f3b15e20c449d4c0ca133
zyswyq <763958570@qq.com>
2017-02-09 14:56:26 +0800

    no message

--- ABaiduMusic/app/src/main/java/com/example/dllo/abaidumusic/MainActivity.java
+++ ABaiduMusic/app/src/main/java/com/example/dllo/abaidumusic/MainActivity.java
@@ -3,14 +3,16 @@ package com.example.dllo.abaidumusic;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+/**
+ * github使用克隆下来的路径
+ */
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        /**
-         * 哈哈哈哈
-         */
+
     }
 }
